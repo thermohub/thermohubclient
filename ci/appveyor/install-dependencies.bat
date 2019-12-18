b3e6ca1d91d0b977266686f68fd416fcf09b7cd2
dmiron <mirondanro@yahoo.com>
2019-12-18 18:56:48 +0100

    fix path

--- ci/appveyor/install-dependencies.bat
+++ ci/appveyor/install-dependencies.bat
@@ -48,7 +48,7 @@ REM cmake -G"Visual Studio 15 2017" -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_BUILD_TYPE=R
 REM echo "Building..."
 REM cmake --build build --config %CONFIGURATION% --target install
 REM 
-cd ..\..\..
+cd ..\..
 echo "Curent directory: %CD%"
 REM 
 REM REM Housekeeping
