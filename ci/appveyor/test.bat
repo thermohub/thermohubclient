f44d0451c35968248f269ec7699153b561db1d6e
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2020-12-16 15:45:54 +0100

    Update test.bat

--- ci/appveyor/test.bat
+++ ci/appveyor/test.bat
@@ -2,4 +2,5 @@ REM call build\test\%CONFIGURATION%\tests.exe
 call conda activate thermohubclient
 call python -c "import thermohubclient"
 call pytest -ra -vv --color=yes .
-if errorlevel 1 exit 1
+if errorlevel <0 ( echo Unknown appveyor exitcode %errorlevel% 
+exit 0 )
